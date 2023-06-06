c7475ba3725e0f473f760079c9436eb83e09536b
ATRVIIE <119594378+ATRVIIE@users.noreply.github.com>
2023-06-06 20:02:28 +0200

    Add files via upload

---
+++ skrill_lamp/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+author 'ATR#4444' 
+
+files {
+  'stream/skrill_lamp_set_props.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/skrill_lamp_set_props.ytyp'
